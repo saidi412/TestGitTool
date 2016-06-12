d1495acd8d30f3ff2ef89264e11de437d981b1fa
Saidi Reddy <saidireddy412@gmail.com>
2016-06-12 18:17:45 +0530

    TestGitTool
    Test Git Tool.

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestGitTool.Startup))]
+namespace TestGitTool
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
